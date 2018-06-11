d36c335605d3f18a99936ec9eec4bb1c1106ba03
Timo Goebel <mail@timogoebel.name>
2018-06-11 16:05:48 +0200

    fixes #23880 - graphql: relay global id

--- app/graphql/types/model.rb
+++ app/graphql/types/model.rb
@@ -2,7 +2,10 @@ module Types
   class Model < GraphQL::Schema::Object
     description 'A Model'
 
-    field :id, Integer, resolve: ->(obj, _, _) { obj.id }, null: false
+    implements GraphQL::Relay::Node.interface
+
+    global_id_field :id
+
     field :name, String, resolve: ->(obj, _, _) { obj.name }, null: false
     field :info, String, resolve: ->(obj, _, _) { obj.info }, null: true
     field :vendorClass, String, resolve: ->(obj, _, _) { obj.vendor_class }, null: true
